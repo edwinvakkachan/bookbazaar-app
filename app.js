b71ec8a16af1c68adb87100c1776d5221f55e3c4
edwin vakkachan <edwinvakkachan@gmail.com>
2025-07-28 17:30:29 +0530

    middleware created

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const env = require('dotenv').config();
+const db = require("./config/db")
+const userRoute = require('./routes/userRoute')
+db()
+
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+app.set("view engine","ejs");
+app.set("views",[path.join(__dirname,"views/user"),path.join(__dirname,'views/admin')])
+app.use(express.static(path.join(__dirname, "public")))  // path corrected should check this
+
+
+
+app.use('/',userRoute)
+
+
+
 
 
 app.listen(process.env.PORT,()=>{
---
+++ config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const env = require('dotenv').config();
+
+
+const connectDB = async ()=>{
+    try{
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log("DB connected successfully");
+    }catch(err){
+        console.error('DB connection error',err.message);
+        process.exit(1);
+    }
+}
+
+module.exports = connectDB;
