c726c8c9be81eaa0eac9dd637712d56b3fb5b204
edwin vakkachan <edwinvakkachan@gmail.com>
2025-07-29 14:22:01 +0530

    sign up page started

--- controllers/user/userController.js
+++ controllers/user/userController.js
@@ -1,3 +1,26 @@
+const loadSignup = async (req,res)=>{
+    try {
+        return res.render('signup')
+    } catch (error) {
+        console.error('signup page rendering failed',error.message)
+        res.status(500).send('signup page rendering error')
+    }
+}
+
+
+
+const pageNotFound = async (req,res)=>{
+    try {
+        return res.render('page-404')
+        
+    } catch (error) {
+        console.error('page not found rendering error',error.message);
+        res.redirect('/pageNotFound')
+    }
+}
+
+
+
 const loadHomepage = async (req,res)=>{
 try {
 
@@ -6,10 +29,12 @@ return res.render('home')
     
 } catch (error) {
     console.error('Home page rendering failed',error.message)
-    res.status(500).send('server error')
+    res.status(500).send('Home page rendering error')
 }
 }
 
 module.exports = {
-    loadHomepage
+    loadHomepage,
+    pageNotFound,
+    loadSignup,
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router();
 const userController = require('../controllers/user/userController')
 
-
+router.get('/pageNotFound',userController.pageNotFound)
 router.get('/',userController.loadHomepage);
+router.get('/signup',userController.loadSignup);
+
 
 
 
